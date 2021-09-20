59e1e5a28e46224cf943dec18f389e2019c2f016
Viana Lin <vianalin@pop-os.localdomain>
2021-09-19 20:57:19 -0400

    added placeholders

---
+++ 02gitonup.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a = 1;
+	long b = 0.991480293485783;
+	float c = 1.25;
+	char d = 'd';
+	printf("%d", a);
+	
+	float sum = a + c;
+	printf("%f *", sum);
+	
+	return 0;
+	}
+	
