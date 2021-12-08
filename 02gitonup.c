56d7d652ec36bd6865dc55a90f9ccd2c74621dfb
Viana Lin <vianalin@pop-os.localdomain>
2021-12-08 12:58:43 -0500

    added things

--- 02gitonup.c
+++ 02gitonup.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 int main() {
+	
+	//use a few different types
 	int a = 1;
 	long b = 0.991480293485783;
 	float c = 1.25;
 	char d = 'd';
-	printf("%d", a);
-	
+	printf("a: %d\n", a);
 	float sum = a + c;
-	printf("%f *", sum);
+	printf("sum: %f\n", sum);
+	
+	//unsigned
+	unsigned int e = 6;
+	unsigned char * f = "c";
+	
+	//out of bounds on either side
+	printf("lower out of bounds: %c\n", d - 200);
+	printf("upper out of bounds: %c\n", d + 200);
 	
 	return 0;
 	}
